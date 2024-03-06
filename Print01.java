06649a0ad37e05c5edbd73f3a1e0c95f3ffea850
florencesy02 <162427539+florencesy02@users.noreply.github.com>
2024-03-06 18:09:35 +0900

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,13 @@
+package sec04;
+
+public class Print01 {
+	public static void main(String[] args) {
+		String myName = "서영";
+		int myAge = 20;
+		System.out.println("이름 : " + myName);
+		System.out.println("나이 : " + myAge);
+		System.out.printf(" 이름 : " + myName + " 나이 : " + myAge);
+
+	}
+
+}
